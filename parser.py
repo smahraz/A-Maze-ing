445dd8312c8f5821d8864df0ec44db399aea1996
saidmakkass <saidmakkass1@gmail.com>
2026-01-22 14:04:39 +0100

    feat(parser): Add invalid option checking, Add Pos[c]
    Parser[c] raises exceptions if mandatory keys are missing or if values are invalid
    Pos[c] represents a cell position

--- parser.py
+++ parser.py
@@ -3,6 +3,23 @@ class ParseError(Exception):
         super().__init__(message)
 
 
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    @classmethod
+    def from_string(cls, s: str):
+        x, y = s.split(",")
+        return cls(int(x), int(y))
+
+
 class Options:
     def __init__(self):
         self.width = None
@@ -28,26 +45,65 @@ class Options:
             raise ParseError("Missing mandatory key: OUTPUT_FILE")
         if self.perfect is None:
             raise ParseError("Missing mandatory key: PERFECT")
+        if self.width < 0:
+            raise ValueError("Invalid WIDTH")
+        if self.height < 0:
+            raise ValueError("Invalid HEIGHT")
+        if self.entry.x >= self.width or self.entry.y >= self.height\
+                or self.entry.x < 0 or self.entry.y < 0:
+            raise ValueError("Invalid ENTRY coordinates")
+        if self.exit.x >= self.width or self.exit.y >= self.height\
+                or self.exit.x < 0 or self.exit.y < 0:
+            raise ValueError("Invalid EXIT coordinates")
 
     def add_option(self, key: str, value):
         match key:
             case "WIDTH":
+                try:
+                    value = int(value)
+                except ValueError:
+                    raise ValueError("WIDTH must be an int value")
                 self.width = value
             case "HEIGHT":
+                try:
+                    value = int(value)
+                except ValueError:
+                    raise ValueError("HEIGHT must be an int value")
                 self.height = value
             case "ENTRY":
+                try:
+                    value = Pos.from_string(value)
+                except ValueError:
+                    raise ValueError("ENTRY must be a Pos value (ex: x,y)")
                 self.entry = value
             case "EXIT":
+                try:
+                    value = Pos.from_string(value)
+                except ValueError:
+                    raise ValueError("EXIT must be a Pos value (ex: x,y)")
                 self.exit = value
             case "OUTPUT_FILE":
                 self.output_file = value
             case "PERFECT":
-                self.perfect = value
+                if value == 'True':
+                    self.perfect = True
+                elif value == 'False':
+                    self.perfect = False
+                else:
+                    raise ValueError("PERFECT must be a True or False")
             case "SEED":
+                try:
+                    value = int(value)
+                except ValueError:
+                    raise ValueError("SEED must be an int value")
                 self.seed = value
             case "ALGORITHM":
+                if value not in ['DFS', 'BFS']:
+                    raise ValueError("ALGORITHM must be DFS or BFS")
                 self.algorithm = value
             case "INTERFACE":
+                if value not in ['gui', 'tui']:
+                    raise ValueError("INTERFACE must be gui or tui")
                 self.interface = value
 
 
